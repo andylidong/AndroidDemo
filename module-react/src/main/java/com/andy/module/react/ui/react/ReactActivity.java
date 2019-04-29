2918f3a9b4cafd4896b64d20ddd47a3e88210765
andylidong <andy_lidong@foxmail.com>
2019-04-29 11:38:53 +0800

    添加react

--- module-react/src/main/java/com/andy/module/react/ui/react/ReactActivity.java
+++ module-react/src/main/java/com/andy/module/react/ui/react/ReactActivity.java
@@ -2,18 +2,91 @@ package com.andy.module.react.ui.react;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
+import android.view.KeyEvent;
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.andy.library.common.router.RouterActivityPath;
-import com.andy.module.react.R;
+import com.facebook.react.BuildConfig;
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactRootView;
+import com.facebook.react.common.LifecycleState;
+import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
+import com.facebook.react.shell.MainReactPackage;
 
 @Route(path = RouterActivityPath.React.PAGER_REACT)
-public class ReactActivity extends AppCompatActivity {
+public class ReactActivity extends AppCompatActivity implements DefaultHardwareBackBtnHandler {
+
+    private final String COMPONENT_NAME = "module-react";
+
+    private ReactRootView mReactRootView;
+    private ReactInstanceManager mReactInstanceManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_react);
+        mReactRootView = new ReactRootView(this);
+        mReactInstanceManager = ReactInstanceManager.builder()
+                .setApplication(getApplication())
+                .setBundleAssetName("index.android.bundle")
+                .setJSMainModulePath("index")
+                .addPackage(new MainReactPackage())
+                .setUseDeveloperSupport(true)
+                .setInitialLifecycleState(LifecycleState.RESUMED)
+                .build();
+
+        //这里的AndroidRnDemoApp必须对应“index.js”中的“AppRegistry.registerComponent()”的第一个参数
+        mReactRootView.startReactApplication(mReactInstanceManager, COMPONENT_NAME, null);
+
+        //加载ReactRootView到布局中
+        setContentView(mReactRootView);
     }
 
+    @Override
+    public void invokeDefaultOnBackPressed() {
+        super.onBackPressed();
+    }
+
+    /**
+     * ReactInstanceManager生命周期同activity
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager.onHostPause(this);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager.onHostResume(this, this);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager.onHostDestroy(this);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager.onBackPressed();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_MENU && mReactInstanceManager != null) {
+            mReactInstanceManager.showDevOptionsDialog();
+            return true;
+        }
+        return super.onKeyUp(keyCode, event);
+    }
 }
