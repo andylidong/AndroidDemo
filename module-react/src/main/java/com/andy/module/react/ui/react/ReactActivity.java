e56a633cf3fdc7a0a60c74e04aeb9967e3462c72
andylidong <andy_lidong@foxmail.com>
2019-04-29 22:21:51 +0800

    修改版本

--- module-react/src/main/java/com/andy/module/react/ui/react/ReactActivity.java
+++
@@ -1,9 +0,0 @@
-package com.andy.module.react.ui.react;
-
-import com.alibaba.android.arouter.facade.annotation.Route;
-import com.andy.library.common.router.RouterActivityPath;
-import com.andy.module.react.base.BaseReactActivity;
-
-@Route(path = RouterActivityPath.React.PAGER_REACT)
-public class ReactActivity extends BaseReactActivity {
-}
