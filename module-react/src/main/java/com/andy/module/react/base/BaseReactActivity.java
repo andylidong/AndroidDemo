093fe120980be0b419cb97581d1f21ba35509756
andylidong <andy_lidong@foxmail.com>
2019-04-29 14:31:25 +0800

    update react

---
+++ module-react/src/main/java/com/andy/module/react/base/BaseReactActivity.java
@@ -0,0 +1,13 @@
+package com.andy.module.react.base;
+
+/**
+ * @Description: TODO
+ * @Author: lidong
+ * @CreateDate: 2019-04-29$ 14:27$
+ * @UpdateUser: lidong
+ * @UpdateDate: 2019-04-29$ 14:27$
+ * @UpdateRemark: TODO
+ * @Version: 1.0
+ **/
+public class BaseReactActivity {
+}
